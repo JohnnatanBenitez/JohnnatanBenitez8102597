9590b3b1ad40fa1011d21eeb8dbeb6f4db28f7de
jonathansanchez2948 <jonathansanchez2948@correo.itm.edu.co>
2024-11-29 18:00:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
-    print("Gracias Profesor Johnnatan por sus Enseñanzas, aprendí demasiado con su metodología."),
+    print("Gracias Profesor Johnnatan por sus Enseñanzas, aprendí demasiado con su metodología.")
+    print("Excelente trabajo")    
  
 if __name__ == "__main__":
     main()
